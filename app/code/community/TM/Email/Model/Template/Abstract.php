11277c3481c3c9eb0f56100828d9cd2ba4678256
Alexander Kras'ko <0m3r.mail@gmail.com>
2014-11-17 17:15:41 +0200

    Aschroder_SMTPPro alpha integration was added (part II)

--- app/code/community/TM/Email/Model/Template/Abstract.php
+++ app/code/community/TM/Email/Model/Template/Abstract.php
@@ -2,8 +2,11 @@
 if (Mage::helper('core')->isModuleOutputEnabled('Aschroder_SMTPPro')) {
     class TM_Email_Model_Template_Abstract extends Aschroder_SMTPPro_Model_Email_Template
     {
-        protected function _beforeSend($transport, $mail)
+        protected function _beforeSend(&$transport, $mail)
         {
+            if (!$transport instanceof Zend_Mail_Transport_Sendmail) {
+                return;
+            }
             $_helper = Mage::helper('smtppro');
             // If it's not enabled, just return the parent result.
             if (!$_helper->isEnabled()) {
