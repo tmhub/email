3f24c56460a6363bc320aca3abc224345e0fe55c
Alexander Kras'ko <0m3r.mail@gmail.com>
2017-09-22 15:28:58 +0300

    reintegrate Aschroder_SMTPPro

--- app/code/community/TM/Email/Model/Template/Abstract.php
+++ app/code/community/TM/Email/Model/Template/Abstract.php
@@ -1,31 +1,11 @@
 <?php
-/*if (Mage::helper('core')->isModuleOutputEnabled('Aschroder_SMTPPro')) {
-    class TM_Email_Model_Template_Abstract extends Aschroder_SMTPPro_Model_Email_Template
-    {
-        protected function _beforeSend(&$transport, $mail)
-        {
-            if (!$transport instanceof Zend_Mail_Transport_Sendmail) {
-                return;
-            }
-            $_helper = Mage::helper('smtppro');
-            // If it's not enabled, just return the parent result.
-            if (!$_helper->isEnabled()) {
-                $_helper->log('SMTP Pro is not enabled, fall back to parent class');
-                return;
-            }
-
-            //observer :: beforeSendTemplate
-            $_helper->log($mail);
-            $transport = $_helper->getTransport();
-        }
-
-    }
+if (Mage::helper('core')->isModuleOutputEnabled('Aschroder_SMTPPro')) {
+    Mage::helper('tmcore')->requireOnce('TM/Email/Model/Template/AschroderSMTPPro.php');
 } else {
-    */
-class TM_Email_Model_Template_Abstract extends Mage_Core_Model_Email_Template
-{
-    protected function _beforeSend($transport, $mail)
+    class TM_Email_Model_Template_Abstract extends Mage_Core_Model_Email_Template
     {
+        protected function _beforeSend($transport, $mail)
+        {
+        }
     }
 }
-// }
---
+++ app/code/community/TM/Email/Model/Template/AschroderSMTPPro.php
@@ -0,0 +1,20 @@
+<?php
+class TM_Email_Model_Template_Abstract extends Aschroder_SMTPPro_Model_Email_Template
+{
+    protected function _beforeSend(&$transport, $mail)
+    {
+        if (!$transport instanceof Zend_Mail_Transport_Sendmail) {
+            return;
+        }
+        $_helper = Mage::helper('smtppro');
+        // If it's not enabled, just return the parent result.
+        if (!$_helper->isEnabled()) {
+            $_helper->log('SMTP Pro is not enabled, fall back to parent class');
+            return;
+        }
+
+        //observer :: beforeSendTemplate
+        $_helper->log($mail);
+        $transport = $_helper->getTransport();
+    }
+}
